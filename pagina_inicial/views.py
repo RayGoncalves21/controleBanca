bc67f0809935f91cdda3a3971c5934108767c62d
RayGoncalves21 <raygoncalves21@gmail.com>
2022-10-06 21:10:33 -0300

    informacoes pagina inicial

--- pagina_inicial/views.py
+++ pagina_inicial/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from bilhetes.forms import AtualizaBilheteForm, BilheteForm
+from bilhetes.models import Bilhete
+from django.contrib import messages
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
+from django.utils import timezone
 
 
 def pagina_inicial(request):
-    return render(request, 'pagina_inicial.html')
+
+    todos_bilhetes = Bilhete.objects.all()
+
+    context = {
+        "nome_pagina": "Inicio da dashboard",
+        'todos_bilhetes': todos_bilhetes,
+
+    }
+    return render(request, "pagina_inicial.html", context)
